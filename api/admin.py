8541335472ffc778791fb5d7b8d5867f03a91762
ChetanSureka <surekachetan09@gmail.com>
2024-01-01 14:41:20 +0530

    deleting all files

--- api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Rooms, Post
-
-admin.site.register(Rooms)
-admin.site.register(Post)
--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PostView, RoomView
-
-
-urlpatterns = [
-    path('room', RoomView.as_view(), name="room"),
-    path('post', PostView.as_view(), name="post"),
-]
